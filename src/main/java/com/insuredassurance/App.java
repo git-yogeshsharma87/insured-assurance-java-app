063f0d401bed0769c8b83510ac81e6959d8ee3d9
Yogesh <yogeshcse05303g@gmail.com>
2025-04-18 03:26:55 +0530

    Initial commit

---
+++ src/main/java/com/insuredassurance/App.java
@@ -0,0 +1,7 @@
+package com.insuredassurance;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Insured Assurance Java Application!");
+    }
+}
